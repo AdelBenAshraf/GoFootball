eddfd9967cd24d102b9d2973a10b5df0fb971704
Adel <37137127+AdelBenAshraf@users.noreply.github.com>
2021-06-11 13:04:40 +0200

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,11 @@
+public interface Person {
+    String name = "";
+    short age = 0;
+    String nationalId = "";
+    String phoneNo = "";
+    String address = "";
+    String sex = "";
+    String userName = "";
+    String email = "";
+    String password = "";
+}
---
+++ Player.java
@@ -0,0 +1,18 @@
+public class Player implements Person
+{
+    PlaygroundOwner requestPlayground()
+    {
+        return ;
+    }
+    PlaygroundOwner bookPlayground()
+    {
+        return;
+    }
+    void displayMyRequests()
+    {
+        for (int i = 0; i < requests.length(); i++)
+        {
+
+        }
+    }
+}
